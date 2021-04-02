07cb41acb9a7f57884aa74a419a11ab22f3aab09
Heikki Peltomäki <heikki.peltomaki@dnainternet.net>
2021-04-03 01:08:17 +0300

    Small testing

--- WPdev/app/public/wp-content/themes/fictional-university-theme/index.php
+++ WPdev/app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,10 @@
-This is our custom theme for testing.
+<?php
+  function greet($name, $color) {
+      echo "<p>Hi, my name is $name and my fav color is $color</p>";
+  }
+
+  greet('John', 'blue');
+  greet('Jane', 'red');
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
